701ebf68597b5e5b69ee40ca681e9e1a3cd8d1da
ChuchinJJ <josedejesuscons@gmail.com>
2021-10-26 14:48:01 -0500

    dev_s05 cambiar tipo de mapa

--- lib/views/lugaresCercanosView.dart
+++ lib/views/lugaresCercanosView.dart
@@ -16,6 +16,8 @@ class LugaresCercanosViewState extends State<LugaresCercanosView> {
   late GoogleMapController mapController;
   late BitmapDescriptor pinPlaceBot;
   Set<Marker> markers = {};
+  String dropdownValue = 'Normal';
+  MapType tipoMapa = MapType.normal;
 
   getPosicion() async {
     await determinePosition().then((position) {
@@ -35,6 +37,20 @@ class LugaresCercanosViewState extends State<LugaresCercanosView> {
   void setCustomMapPin() async {
     pinPlaceBot = await BitmapDescriptor.fromAssetImage(
         ImageConfiguration(), 'assets/pinPlaceBot.png');
+  }
+
+  void cambiarMapa(String tipo) {
+    MapType mapa;
+    if (tipo == "Terreno") {
+      mapa = MapType.terrain;
+    } else if (tipo == "Satelite") {
+      mapa = MapType.satellite;
+    } else {
+      mapa = MapType.normal;
+    }
+    setState(() {
+      tipoMapa = mapa;
+    });
   }
 
   @override
@@ -67,19 +83,48 @@ class LugaresCercanosViewState extends State<LugaresCercanosView> {
       }
     }
 
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 15.0,
+    return Stack(children: [
+      GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 15.0,
+        ),
+        markers: Set<Marker>.from(markers),
+        mapType: tipoMapa,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
+        rotateGesturesEnabled: false,
       ),
-      markers: Set<Marker>.from(markers),
-      mapType: MapType.terrain,
-      myLocationEnabled: true,
-      myLocationButtonEnabled: true,
-      zoomGesturesEnabled: true,
-      zoomControlsEnabled: true,
-      rotateGesturesEnabled: false,
-    );
+      Positioned(
+          right: 10,
+          top: 80,
+          child: DropdownButton<String>(
+            icon: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.deepOrange, shape: BoxShape.circle),
+                child: Icon(Icons.map, color: Colors.white)),
+            iconSize: 40,
+            underline: Text(''),
+            dropdownColor: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+                cambiarMapa(newValue);
+              });
+            },
+            items: <String>['Normal', 'Satelite', 'Terreno']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )),
+    ]);
   }
 }
--- lib/views/lugarview.dart
+++ lib/views/lugarview.dart
@@ -15,10 +15,26 @@ class LugarViewState extends State<LugarView> {
   late GoogleMapController mapController;
   late BitmapDescriptor pinPlaceBot;
   Set<Marker> markers = {};
+  String dropdownValue = 'Normal';
+  MapType tipoMapa = MapType.normal;
 
   void setCustomMapPin() async {
     pinPlaceBot = await BitmapDescriptor.fromAssetImage(
         ImageConfiguration(), 'assets/pinPlaceBot.png');
+  }
+
+  void cambiarMapa(String tipo) {
+    MapType mapa;
+    if (tipo == "Terreno") {
+      mapa = MapType.terrain;
+    } else if (tipo == "Satelite") {
+      mapa = MapType.satellite;
+    } else {
+      mapa = MapType.normal;
+    }
+    setState(() {
+      tipoMapa = mapa;
+    });
   }
 
   @override
@@ -50,19 +66,48 @@ class LugarViewState extends State<LugarView> {
       });
     }
 
-    return GoogleMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 18.0,
+    return Stack(children: [
+      GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 18.0,
+        ),
+        markers: Set<Marker>.from(markers),
+        mapType: tipoMapa,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
+        rotateGesturesEnabled: false,
       ),
-      markers: Set<Marker>.from(markers),
-      mapType: MapType.terrain,
-      myLocationEnabled: true,
-      myLocationButtonEnabled: true,
-      zoomGesturesEnabled: true,
-      zoomControlsEnabled: true,
-      rotateGesturesEnabled: false,
-    );
+      Positioned(
+          right: 10,
+          top: 80,
+          child: DropdownButton<String>(
+            icon: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    color: Colors.deepOrange, shape: BoxShape.circle),
+                child: Icon(Icons.map, color: Colors.white)),
+            iconSize: 40,
+            underline: Text(''),
+            dropdownColor: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+                cambiarMapa(newValue);
+              });
+            },
+            items: <String>['Normal', 'Satelite', 'Terreno']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )),
+    ]);
   }
 }
